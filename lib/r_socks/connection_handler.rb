e42874ad91491d0ef1fe565b8d124775c20f2cfc
Nick An <anning0322@gmail.com>
2020-03-17 22:32:23 +1100

    add handshake logic

--- lib/r_socks/connection_handler.rb
+++ lib/r_socks/connection_handler.rb
@@ -1,13 +1,21 @@
 require 'socket'
 require 'eventmachine'
 require 'socket'
+require './r_socks/state_machine'
 
 module RSocks
-  class ConnectionHandler < EM::Connection
 
+  VERSION = 0x05
+  NOT_ACCEPT = 0xFF
+
+  PASSWORD_LOGIN = 0x02
+  NO_AUTH = 0x00
+
+  class ConnectionHandler < EM::Connection
     def post_init
       @port, @ip = Socket.unpack_sockaddr_in(get_peername)
       puts "new #{@ip}:#{@port} connected."
+      @state_machine = RSocks::StateMachine.new
     end
 
 
@@ -15,13 +23,49 @@ module RSocks
 
     def receive_data(data)
       p data
-      verison, num, method = data.unpack('CCC')
-      send_data [verison, method].pack('CC')
+      if @state_machine.handshake?
+        send_data(init_handshake(data))
+      end
+
+      if @state_machine.auth?
+        #working on
+      end
+
+      if @state_machine.start?
+        #working on
+      end
       # working on
     end
 
     def unbind
       puts "#{@ip}:#{@port} had disconnected."
+    end
+
+    private
+
+    def init_handshake(data)
+
+      version, num = data.unpack('CC')
+
+      if version != RSocks::VERSION || num <= 0
+        return [RSocks::VERSION, RSocks::NOT_ACCEPT].pack('CC')
+      end
+
+      position = 'C' * num
+
+      methods = data[2..-1].unpack(position)
+
+      if methods.include?(PASSWORD_LOGIN)
+        @state_machine.auth!
+        return [RSocks::VERSION, PASSWORD_LOGIN].pack('CC')
+      end
+
+      if methods.include?(NO_AUTH)
+        @state_machine.start!
+        return [RSocks::VERSION, NO_AUTH].pack('CC')
+      end
+
+      return [RSocks::VERSION, RSocks::NOT_ACCEPT].pack('CC')
     end
   end
 end
--- lib/r_socks/state_machine.rb
+++ lib/r_socks/state_machine.rb
@@ -1,8 +1,42 @@
 module RSocks
 
-  STATE_LIST = [:begin, :auth, :start]
+  STATE_LIST = [:handshake, :auth, :start]
 
   class StateMachine
 
+    attr_reader :current_state
+
+    def initialize
+      @max = 2
+      @state_num = 0
+      @current_state = RSocks::STATE_LIST[@state_num]
+    end
+
+    def handshake?
+      current_state == :handshake
+    end
+
+    def auth?
+      current_state == :auth
+    end
+
+    def start?
+      current_state == :start?
+    end
+
+    def auth!
+      @state_num = 1
+      @current_state = RSocks::STATE_LIST[@state_num]
+    end
+
+    def start!
+      @state_num = 2
+      @current_state = RSocks::STATE_LIST[@state_num]
+    end
+
+    def state_changed
+      @state_num += 1 unless @state_num >= @max
+      @current_state = RSocks::STATE_LIST[@state_num]
+    end
   end
 end
